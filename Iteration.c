86dd736d5a34de119ac4598101e1209dbd3fbe42
Byeonseoyoon <bsy4943@naver.com>
2021-12-20 20:14:06 +0900

    binsearch 반복문

---
+++ Iteration.c
@@ -0,0 +1,32 @@
+#pragma warning(disable: 4996)
+#include<stdio.h>
+
+int binsearch(int arr[], int target, int low, int high) {
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (target == arr[mid]) return mid;
+		else if (target < arr[mid]) high = mid - 1;
+		else low = mid + 1;
+	}
+	return -1;
+}
+
+int main(void) {
+
+	int input;
+	scanf("%d", &input);
+
+	int arr[100] = { 0 };
+
+	for (int i = 0; i < input; i++) scanf("%d", &arr[i]);
+
+	int target;
+	scanf("%d", &target);
+
+	int index = binsearch(arr, target, 0, input - 1);
+
+	if (index == -1) printf("½ÇÆÐ");
+	else printf("%d", index+1);
+
+	return 0;
+}
